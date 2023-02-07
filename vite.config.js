09517cb32900d6f673a0cfb4c8752a2da1c0059a
Ameya Deo <122508209+ADeo123@users.noreply.github.com>
2023-02-06 22:16:33 -0500

    Second Iteration

--- vite.config.js
+++ vite.config.js
@@ -6,39 +6,40 @@ export default defineConfig({
   plugins: [vue()]
 })
 
+//Duplicates the card using vue 
 vue.querySelector(".duplicateBtn").addEventListener("click", function(e){
-    var div = vue.querySelector(".duplicateBtn");
+    const div = vue.querySelector(".duplicateBtn");
     let division = div.cloneNode(true);
     vue.body.appendChild(division);
 });
 
 //JavaScript Hover implementation
-document.querySelector("div").addEventListener("mouseover", function (e) {
-  const div = document.querySelector("div");
+vue.querySelector("div").addEventListener("mouseover", function (e) {
+  const div = vue.querySelector("div");
   let hoverEffect = div.style.borderColor = "white";
-  document.body.appendChild(hoverEffect);
+  vue.body.appendChild(hoverEffect);
 });
 
-document.querySelector("div").addEventListener("mouseout", function (e) {
-  const div = document.querySelector("div");
+vue.querySelector("div").addEventListener("mouseout", function (e) {
+  const div = vue.querySelector("div");
   let hoverEffect = div.style.borderColor = "black";
-  document.body.appendChild(hoverEffect);
+  vue.body.appendChild(hoverEffect);
 });
 
 //When the card is clicked the color changes
-document.querySelector("changeColorBtn").addEventListener("click", function (e) {
-  const div = document.querySelector("div");
+vue.querySelector("changeColorBtn").addEventListener("click", function (e) {
+  const div = vue.querySelector("div");
   let divColor = (div.style.backgroundColor = "gray"); //changes the background to gray
-  document.body.appendChild(divColor);
+  vue.body.appendChild(divColor);
 });
 
 //When the heading/title is clicked it is change to "something else"
-document.querySelector("ChangeHeadingBtn").addEventListener("click", function (e) {
+vue.querySelector("ChangeHeadingBtn").addEventListener("click", function (e) {
   heading.innerHTML = "Something else"; //replaces the text of the heading
 });
 
 //Deletes the last duplicated card from the webpage
-document.querySelector(".deleteBtn").addEventListener("click", function (e) {
-  let div = document.querySelector("div");
+vue.querySelector(".deleteBtn").addEventListener("click", function (e) {
+  let div = vue.querySelector("div");
   div.parentNode.removeChild(div);
 });
